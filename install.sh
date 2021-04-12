4f190f73cdf2e30269238b7991c5f381b570eb82
Erik Aumayr <erik.aumayr@ericsson.com>
2021-04-12 12:56:24 +0100

    Updated version numbers of images that have the updated package dependencies

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-docker build --rm -t 5genesis-analytics/data-handler:0.2.6 "Data handler"
-docker build --rm -t 5genesis-analytics/correlation:0.1.4 Correlation
-docker build --rm -t 5genesis-analytics/prediction:0.1.6 Prediction
-docker build --rm -t 5genesis-analytics/statistical-analysis:0.1.3 "Statistical analysis"
-docker build --rm -t 5genesis-analytics/feature-selection:0.1.2 Feature_Selection
-docker build --rm -t 5genesis-analytics/visualization:1.0.0 Visualization
+docker build --rm -t 5genesis-analytics/data-handler:0.2.7 "Data handler"
+docker build --rm -t 5genesis-analytics/correlation:0.1.5 Correlation
+docker build --rm -t 5genesis-analytics/prediction:0.1.7 Prediction
+docker build --rm -t 5genesis-analytics/statistical-analysis:0.1.4 "Statistical analysis"
+docker build --rm -t 5genesis-analytics/feature-selection:0.1.3 Feature_Selection
+docker build --rm -t 5genesis-analytics/visualization:1.0.1 Visualization
 docker stack deploy -c analytics-stack.yaml analytics
